0c1c8492e67e295117146ee0720f3e97a6735a4b
Chaoching Vang <chaoching.vang@gmail.com>
2021-10-04 13:00:21 -0500

    added comments

--- client.js
+++ client.js
@@ -7,12 +7,16 @@ let totalMonthly = 0;
 
 function jqLoaded() {
     $(`#submitButton`).on(`click`, addEmployee);
+    //$(document).on(`click`, `#deleteButton`, deleteEmployee) will search for in whole document
     $(`#tableOutput`).on(`click`, `#deleteButton`, deleteEmployee);
     displayOnDOM();
 }
 
+
 function addEmployee() {
     console.log('in addEmployee fx');
+
+    //Taking in inputs and adding in the inputs as an employee object
     let employee = {
         firstName: $(`#firstNameInput`).val(),
         lastName: $(`#lastNameInput`).val(),
@@ -21,10 +25,13 @@ function addEmployee() {
         annualSalary: $(`#annualSalaryInput`).val()
     }
     // console.log(employee);
+
+    //Putting the employee info into our list of employee array
     employeeList.push(employee);
 
     displayOnDOM();
 
+    //emptying the input values on the DOM
     employee = {
         firstName: $(`#firstNameInput`).val(``),
         lastName: $(`#lastNameInput`).val(``),
@@ -40,6 +47,8 @@ function addEmployee() {
 function deleteEmployee() {
     // console.log($(this).parent().val());
     // console.log($(this).closest(`tr`).val());
+
+    //removes the row when delete button is clicked on that row
     $(this).closest(`tr`).remove();
 }
 
@@ -48,6 +57,8 @@ function deleteEmployee() {
 function displayOnDOM() {
     $(`#tableOutput`).empty();
 
+
+    //loop through employeeList array and append it to the DOM
     for (let employee of employeeList) {
         let employeeRowOutput = $(`
             <tr>
@@ -63,6 +74,8 @@ function displayOnDOM() {
         $(`#tableOutput`).append(employeeRowOutput);
     }
 
+
+    //Emptying total monthly calc and updating it with new info
     $(`#totalMonthly`).empty();
     calcTotalMonthly();
 
